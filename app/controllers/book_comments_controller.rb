658b79fa1c26a5f4748be5bc8a1d849fd675bddd
ryota35 <ryota35.1025@gmail.com>
2021-03-09 06:37:16 +0000

    [Finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referer
+    render :index
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment =BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
